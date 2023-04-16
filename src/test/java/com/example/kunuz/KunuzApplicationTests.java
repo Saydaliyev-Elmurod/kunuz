67befd76ff67b4a0452e2a9d4602c6a4aba1436f
Elmurod2203 <saydaliyevelmurod01@gmail.com>
2023-04-16 10:35:26 +0500

    start project

---
+++ src/test/java/com/example/kunuz/KunuzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kunuz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KunuzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
